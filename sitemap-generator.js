65269239ec5b3b720277c695f1dc2fa699645442
13thirteen-sudo <115991612+13thirteen-sudo@users.noreply.github.com>
2025-04-03 18:30:17 +0100

    Create sitemap-generator.js

---
+++ sitemap-generator.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+async function generate() {
+  const res = await fetch("https://journal.yumi.co.uk/wp-json/wp/v2/posts?per_page=100&_embed");
+  const posts = await res.json();
+
+  const xmlItems = posts.map((post) => {
+    return `
+  <url>
+    <loc>https://yumi.co.uk/pages/journal/${post.slug}</loc>
+    <lastmod>${post.modified || post.date}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+  });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${xmlItems.join("\n")}
+</urlset>`;
+
+  fs.writeFileSync("sitemap.xml", sitemap);
+}
+
+generate();
